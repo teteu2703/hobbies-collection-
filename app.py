51fa8c4883b0768e3a0f761d44f506ecd605bc6c
teteu2703 <t3t3u0327@gmail.com>
2024-08-15 12:12:31 -0300

    aula 15/08

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 API_URL = 'http://127.0.0.1:5000/api/livros'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        titulo = request.form.get('titulo')
+        autor = request.form.get('autor')
+
+        response = requests.post(API_URL, json={'titulo':titulo, 'autor':autor})
+
+
     response = requests.get(API_URL)
     if response.status_code == 200:
         livros = response.json()
